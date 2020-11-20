61527a2709b3ef93ecab1303f2994b00474013e3
hchurch11 <heatherlchurch11@gmail.com>
2020-11-20 16:55:27 -0700

    updated open source link

--- src/Footer.js
+++ src/Footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
   return (
     <div className="Footer text-center ">
       <p>
-        <a href="https://github.com/hchurch11/to-do">open-sourced</a> code by
-        Heather Church
+        <a href="https://github.com/hchurch11/react-to-do">open-sourced</a> code
+        by Heather Church
         <img src={logo} className="App-logo" alt="logo" />
       </p>
     </div>
